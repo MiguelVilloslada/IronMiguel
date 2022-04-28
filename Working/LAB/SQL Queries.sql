0003b7ffd25fb00f7a8ee55b7c30bac5600cf0f7
MiguelVilloslada <mvillosladarodriguez@gmail.com>
2022-04-28 09:48:26 +0200

    LAB SQL 28_04

---
+++ Working/LAB/SQL Queries.sql
@@ -0,0 +1,22 @@
+use sakila
+
+#Using the rental table, find out how many rentals were processed by each employee.
+select * from rental;
+Select count(rental_id) as rental_processed, staff_id from rental
+group by staff_id;
+
+#Using the film table, find out how many films were released.
+select * from film;
+Select count(distinct(title)) as films_released from film
+where release_year is not null;
+
+#Using the film table, find out how many films there are of each rating. Sort the results in descending order of the number of films.
+Select count(distinct(title)), rating from film
+group by rating;
+
+#Which kind of movies (rating) have a mean duration of more than two hours?
+select * from film;
+Select distinct(rating), special_features from film;
+where avg(length)>120
+group by special_features
+
